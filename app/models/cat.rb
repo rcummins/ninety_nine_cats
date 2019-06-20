c7d53658d3864463ae64815255c5e530aa79a2db
Renata Cummins <renatacummins@gmail.com>
2019-06-20 17:36:16 -0400

    Add migration and model for cats table

---
+++ app/models/cat.rb
@@ -0,0 +1,11 @@
+class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+
+    validates :color, inclusion: { in: %w(black brown white orange) }
+    validates :sex, inclusion: { in: %w(M F) }
+    validates :birth_date, :color, :name, :sex, :description, presence: true
+    
+    def age
+        time_ago_in_words(self.birth_date)
+    end
+end
