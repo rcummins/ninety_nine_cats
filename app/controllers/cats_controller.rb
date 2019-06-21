4c3daee134862398e9cff4a06a7d37136b69a207
Renata Cummins <renatacummins@gmail.com>
2019-06-21 08:41:26 -0400

    feature: add #show action and view for cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,4 +3,14 @@ class CatsController < ApplicationController
         @cats = Cat.all
         render :index
     end
+
+    def show
+        @cat = Cat.find_by(id: params[:id])
+
+        if @cat
+            render :show
+        else
+            redirect_to cats_url
+        end
+    end
 end
